be08ff3892d0cf26e71d31a9b24bc36d5d0b92a7
ramoncordeiro <ramonestii@gmail.com>
2022-12-27 12:26:28 -0300

    uploading files

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x34402996de27CD2F368DB7f8e78389D122fc8a8D";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
